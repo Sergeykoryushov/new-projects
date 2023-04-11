b88be82375ee7af68bd91e114bf171dbcba657f9
Sergey Koryushov <lala170392@mail.ru>
2023-04-11 21:33:24 +0300

    'delete'

--- index.html/src/main/java/org/example/Main.java
+++ index.html/src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
 
+import java.text.Format;
 import java.time.LocalDateTime;
+import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now().plusHours(10));
     }
 }
