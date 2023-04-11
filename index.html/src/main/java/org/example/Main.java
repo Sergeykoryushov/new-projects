4cea937aecd029b486506229e60c19b41a0038e9
Sergey Koryushov <lala170392@mail.ru>
2023-04-11 22:16:13 +0300

    'aaa'

--- index.html/src/main/java/org/example/Main.java
+++ index.html/src/main/java/org/example/Main.java
@@ -3,14 +3,9 @@ package org.example;
 
 import java.text.Format;
 import java.time.LocalDateTime;
-import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now().plusHours(10));
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + (i * 3));
-
-        }
+        System.out.println(LocalDateTime.now().plusHours(3));
     }
 }
